b928390a41f45fd419c12f55cc7621801e2817e8
gitsubasa <tsubasa.adachi@outlook.com>
2024-12-15 16:00:13 +0800

    First upload to git in streamlit lesson.

---
+++ x_test_streamlit1.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+#Write title
+st.title("Hello, Tsubasa :penguin:")
+
+st.write("I'm tring a website creation by using Streamlit in Python. From now on, several feather in streamlit are shown in this page.")
+
+
